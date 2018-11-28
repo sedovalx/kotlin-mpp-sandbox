1cb5023541ced8ab983156634057596407917584
Alexander Sedov <alexander.sedov@jetbrains.com>
2018-11-28 19:35:59 +0300

    Added run configurations for tests

---
+++ subprojects/model/src/jsTest/kotlin/sample/AnotherTestsJS.kt
@@ -0,0 +1,17 @@
+package sample
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class AnotherTestsJS {
+    @Test
+    fun oneMoreTest() {
+        assertNotNull("")
+    }
+
+    @Test
+    fun oneMoreMoreTest() {
+        assertNull(null)
+    }
+}
--- subprojects/model/src/jsTest/kotlin/sample/SampleTestsJS.kt
+++ subprojects/model/src/jsTest/kotlin/sample/SampleTestsJS.kt
@@ -1,11 +1,17 @@
 package sample
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class SampleTestsJS {
     @Test
     fun testHello() {
         assertTrue("JS" in hello())
+    }
+
+    @Test
+    fun anotherTest() {
+        assertEquals("1", "1")
     }
 }
